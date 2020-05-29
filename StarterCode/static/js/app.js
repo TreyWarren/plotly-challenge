479f87286808477f8bbebfc94b612995653f765b
bae elson <baeelson@baes-Air.fios-router.home>
2020-05-29 02:46:04 -0400

    added comments

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,39 +1,54 @@
-
-function init() {
+// INIT FUNCTION //////////////////////////////////////////////////////////////////////
+function init(callback) {
+    // Select the dropdown menu
     var dropdownMenu = d3.select("#selDataset");
     // Use D3.json() to fetch and read the JSON file
-    d3.json("../../data/samples.json").then((importedData) => {
-        var data = importedData;
-        console.log(data);
-    
-        console.log(data.names)
+    d3.json("../../data/samples.json").then((data) => {
+        // Append each one of our subject IDs to the dropdown
         data.names.forEach(element => {
             dropdownMenu.append("option").attr("value", element).text(element);
-    
         });
     });
+    
+    callback();
 };
 
 
-
-
-
-// On change to the DOM, call getData()
+// On change, call getData()
 d3.selectAll("#selDataset").on("change", getData);
 
-// Function called by DOM changes
+
+// GETDATA FUNCTION //////////////////////////////////////////////////////////////////
 function getData() {
+    // Select the dropdown menu
     var dropdownMenu = d3.select("#selDataset");
     // Assign the value of the dropdown menu option to a variable
     var subject_selection = dropdownMenu.property("value");
-  
-
     // Use D3.json() to fetch and read the JSON file
-    d3.json("../../data/samples.json").then((importedData) => {
-        // console.log(importedData);
-        var data = importedData;
-        // console.log(data.metadata);
+    d3.json("../../data/samples.json").then((data) => {
+        
+        // DEMOGRAPHIC INFO //////////////////////////////////////////////////////////
+        // Select the Demographic Info div
+        var demographicInfo = d3.select("#sample-metadata");
+        // Clear any existing data
+        demographicInfo.html("");
+        
+        // // Get the Metadata for our specific subject
+        // var metadata = data.metadata.filter(item => item.id == subject_selection)[0]
+        // // Display our key-value pairs 
+        // Object.entries(metadata).forEach(([key, value]) => {
+        //     // Append a new paragraph for each piece of data
+        //     var row = demographicInfo.append("p");
+        //     // Fill said paragraph with our data (and capitalize the key)
+        //     row.text(`${key.charAt(0).toUpperCase()+key.slice(1)}: ${value}`);
+        // });
+
+        // Although the following code isn't as elegant as the above (which accomplishes what was necessary),
+        // I preferred the following technique simply because it labeled the data more clearly (Washing Frequency vs Wfreq)
+
+        // Get the index of the Subject ID making it easier to call the rest of the data
         var index = data.names.findIndex((id) => id == subject_selection)
+        // Get our data to populate the Demographic Info
         var subject_id = data.metadata[index].id;
         var ethnicity = data.metadata[index].ethnicity;
         var gender = data.metadata[index].gender;
@@ -41,11 +56,10 @@ function getData() {
         var location = data.metadata[index].location;
         var bbtype = data.metadata[index].bbtype;
         var wfreq = data.metadata[index].wfreq;
-
-        console.log(subject_id);
-        console.log(index);
-
-        var demographicInfo = d3.select("#sample-metadata");
+        // Check
+        console.log(`${index}: Subject ID #${subject_id}`);
+    
+        // Display our data
         demographicInfo.html(  `ID: ${subject_id}<br>
                                 Ethnicity: ${ethnicity}<br>
                                 Gender: ${gender}<br>
@@ -53,38 +67,35 @@ function getData() {
                                 Location: ${location}<br>
                                 Navel Type: ${bbtype}<br>
                                 Washing Frequency: ${wfreq}`
-                            );
-        /////////////////////////////////////////////////////////////////
+        );
         
+        // BAR CHART ////////////////////////////////////////////////////////////
+        
+        //  Get our data to populate the bar chart
         var sample_list = data.samples[index];
         var sample_values = sample_list.sample_values;
         var otu_ids = sample_list.otu_ids;
         var otu_labels = sample_list.otu_labels;
 
         // Sort the data array using the greekSearchResults value
-        sample_values.sort(function(a, b) {
-            return parseFloat(b) - parseFloat(a);
-        });
+        sample_values.sort((a, b) => {parseFloat(b) - parseFloat(a);});
 
         // Slice the first 10 objects for plotting
-        var ten = [9,8,7,6,5,4,3,2,1,0]
         var sample_values_sliced = sample_values.slice(0, 10);
         var otu_ids_sliced = otu_ids.slice(0, 10).map(id => `OTU ${id}`);
         var otu_labels_sliced = otu_labels.slice(0, 10);
+        var ten = [...Array(10).keys()].reverse() // simply creates the array [9,8,7,6,5,4,3,2,1,0]
 
-        // Trace for the Data
-        var bar_trace = {
+        // Bar Data
+        var bar_data = [{
             x: sample_values_sliced,
             y: ten,
             text: otu_labels_sliced,
             type: "bar",
             orientation: "h"
-        };
+        }];
 
-        // data
-        var bar_data = [bar_trace];
-
-        // Apply the group bar mode to the layout
+        // Bar Layout 
         var bar_layout = {
             title: `Bacteria Found in<br>Subject ${subject_id}'s Belly Button`,
             yaxis: {tickmode: "array",
@@ -93,12 +104,13 @@ function getData() {
             }
         };
 
-        // Render the plot to the div tag with id "plot"
+        // Render the plot to the div tag with id "bar"
         Plotly.newPlot("bar", bar_data, bar_layout, {responsive: true});
         
-        ////////////////////////////////////////////////////////////////////////////////
-
-        var bubble_trace = {
+        // BUBBLE CHART /////////////////////////////////////////////////////////
+        
+        // Bubble Data
+        var bubble_data = [{
             x: otu_ids,
             y: sample_values,
             text: otu_labels,
@@ -109,12 +121,9 @@ function getData() {
             // My colors by dafault go from grey(0) to red(∞), making the first value negative for some reason makes the colors go from purple(-∞) to grey to red(∞)
             color: otu_ids.map(id => id-Math.min(...otu_ids)-1)
             }
-        };
-        console.log(Math.max(...sample_values));
-        console.log(Math.min(...sample_values));
+        }];
 
-        var bubble_data = [bubble_trace];
-        
+        // Bubble Layout
         var bubble_layout = {
             title: `Bubble Chart for Subject ${subject_id}`,
             xaxis: {
@@ -125,9 +134,11 @@ function getData() {
             }
         };
         
+        // Render the plot to the div tag with id "bubble"
         Plotly.newPlot("bubble", bubble_data, bubble_layout, {responsive: true});
 
     });
 }
-init();
-getData();
+
+init(getData); // unsure why this doesn't work...
+setTimeout(getData, 500); // so I have to put a delay on getData()
